3dc61a999de3f2f539231e87ad60015031a8ea04
Jonathan Quebral <jonathan.almodiel.quebral@gmail.com>
2016-12-21 02:37:45 +0800

    Version 2.0

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'users';
+$route['default_controller'] = 'Login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+// Login Routes
+$route['login'] = 'Login/Index';
+$route['login/auth_login'] = 'Login/Auth_login';
+
+// Logout Route
+$route['logout'] = 'Logout/Index';
+
+// Admin Routes
+$route['admin/dashboard'] = 'Admin_Dboard/Dashboard/Index';
+$route['admin/acct_mngr'] = 'Account_Manager/Account_manager/Index';
